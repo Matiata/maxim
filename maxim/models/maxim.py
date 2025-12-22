a84b1c034399600ec8fa74cb2b86c337c982de12
Matiata <matizmurat@gmail.com>
2025-12-22 20:12:56 -0300

    debugging print

--- maxim/models/maxim.py
+++ maxim/models/maxim.py
@@ -876,6 +876,8 @@ class MAXIM(nn.Module):
       # Store outputs
       outputs_all.append(outputs)
 
+      print(f"maxim return_features: {return_features}, final_features shape: {final_features.shape if final_features is not None else None}")
+
       if return_features:
         # returning final features instead of outputs allows to connect with MoE Router
         return final_features
