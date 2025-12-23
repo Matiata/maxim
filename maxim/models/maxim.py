d1a965ddfdc50175e00f69c8956866215c31e405
Matiata <matizmurat@gmail.com>
2025-12-23 17:38:17 -0300

    adjust conditions

--- maxim/models/maxim.py
+++ maxim/models/maxim.py
@@ -852,7 +852,8 @@ class MAXIM(nn.Module):
         decs.append(x)
         # set final features for return_features=True
         final_features = None
-        if idx_stage == self.num_stages - 1 and i == 0:
+        print(f"idx_stage: {idx_stage}, i: {i}, depth: {self.depth}, num_stages: {self.num_stages}, x shape: {x.shape}")
+        if idx_stage == self.num_stages - 1 and i == self.depth - 1:
           final_features = x
 
         # output conv, if not final stage, use supervised-attention-block.
