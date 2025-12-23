2d6adbabc194c5cc54a85a68bc7ab1b4adf99aec
Matiata <matizmurat@gmail.com>
2025-12-23 15:34:12 -0300

    debugging final_features

--- maxim/models/maxim.py
+++ maxim/models/maxim.py
@@ -854,6 +854,7 @@ class MAXIM(nn.Module):
         final_features = None
         if idx_stage == self.num_stages - 1 and i == 0:
           final_features = x
+        print(f"final_features: {final_features}, idx_stage:{idx_stage}, i:{i}")
         # output conv, if not final stage, use supervised-attention-block.
         if i < self.num_supervision_scales:
           if idx_stage < self.num_stages - 1:  # not last stage, apply SAM
